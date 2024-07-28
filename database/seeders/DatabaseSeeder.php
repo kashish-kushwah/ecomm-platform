70bbd8470edb73801e3007aaa3fcf38dc1fa3f23
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2024-07-28 13:03:09 +0530

    updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,17 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'admin@localhost.com',
+            'role' => 'admin'
+        ]);
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test One',
+            'email' => 'test1@localhost.com',
+            'role' => 'registered'
+        ]);
+
     }
 }
