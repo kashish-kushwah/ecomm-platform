a0aa4b29b07fda88fc52f36640796cf1596d0d93
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2024-08-13 14:31:35 +0530

    update search

--- app/Http/Controllers/front/ShopController.php
+++ app/Http/Controllers/front/ShopController.php
@@ -5,22 +5,37 @@ namespace App\Http\Controllers\front;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ShopController extends Controller
 {
     public function show(Product $product)
     {
-       // $product = Product::findOrFail($product->Id);
+        // $product = Product::findOrFail($product->Id);
         return view('front.product.show', ['product' => $product]);
     }
 
-    public function search(Request $request){
+    public function search(Request $request)
+    {
         $term = $request->term;
         $query = Product::query();
-        if($term){
+        if ($term) {
             $query->where('title', 'like', '%' . $term . '%');
         }
+
+        if ($request->min != "") {
+            $query->where(
+                'price',
+                '>=',
+                $request->min,
+            )->where(
+                'price',
+                '<=',
+                $request->max,
+            );
+        }
+        //echo $query->toRawSql();
         $products = $query->paginate(12);
-        return view('front.product.search', ['products' => $products,'term' => $term]);
-    }   
+        return view('front.product.search', ['products' => $products, 'term' => $term]);
+    }
 }
--- resources/views/front/product/search.blade.php
+++ resources/views/front/product/search.blade.php
@@ -3,24 +3,46 @@
   <div class="padding-large container">
     <h3>Search result for <b>{{ $term }}</b></h3>
     <hr class="mb-5">
-    <div class="d-flex align-items-center justify-content-between flex-wrap gap-1">
-      @forelse ($products as $product)
-        <div class="card mt-3 mb-3" style="width: 23%">
-          <div class="card-body">
-            <h5 class="card-title">{{ substr($product->description, 0, 20) . '...' }}</h5>
-            <p class="card-text">Price: {{ $product->price }}</p>
-            <p class="card-text">Description: {{ substr($product->description, 0, 50) . '...' }}</p>
-            <p class="text-center"><a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">Read
-                More</a></p>
-          </div>
+    <div class="row">
+      <div class="col-3">
+        <form method="get" action="{{ route('front.product.search') }}">
+          <input type="hidden" name="term" value="{{request()->get('term')}}">
+          <table>
+            <tr>
+              <td><input required type="text" name="min" placeholder="Min" value="{{ request()->get('min') }}"></td>
+            </tr>
+            <tr>
+              <td><input required type="text" name="max" placeholder="Max" value="{{ request()->get('max') }}"></td>
+            </tr>
+            <tr>
+            <td><button type="submit" class="search-submit">Filter</button></td>
+            </tr>
+          </table>
+          
+        </form>
+      </div>
+      <div class="col-9">
+        <div class="d-flex align-items-center justify-content-between flex-wrap gap-1">
+          @forelse ($products as $product)
+            <div class="card mt-3 mb-3" style="width: 23%">
+              <div class="card-body">
+                <h5 class="card-title">{{ substr($product->description, 0, 20) . '...' }}</h5>
+                <p class="card-text">Price: {{ $product->price }}</p>
+                <p class="card-text">Description: {{ substr($product->description, 0, 50) . '...' }}</p>
+                <p class="text-center"><a href="{{ route('products.show', $product->id) }}" class="btn btn-primary">Read
+                    More</a></p>
+              </div>
 
+            </div>
+          @empty
+            <div class="col">No items found</div>
+          @endforelse
         </div>
-      @empty
-        <div class="col">No items found</div>
-      @endforelse
+        <div class="row mt-3">
+          <div class="col-12">{{ $products->withQueryString()->links() }}</div>
+        </div>
+      </div>
     </div>
-    <div class="row mt-3">
-      <div class="col-12">{{ $products->withQueryString()->links() }}</div>
-    </div>
+
   </div>
 @endsection
